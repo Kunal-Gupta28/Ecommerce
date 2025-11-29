e9410bd92da3beb4a412730d53c87a7d7ca11782
Kunal-Gupta28 <kunal.gmail.91165@gmail.com>
2025-11-29 18:18:57 +0530

    removing

--- src/themeLoader.js
+++
@@ -1,3 +0,0 @@
-// Apply theme before React loads to prevent flashing
-const storedTheme = localStorage.getItem("theme") || "light";
-document.documentElement.classList.toggle("dark", storedTheme === "dark");
